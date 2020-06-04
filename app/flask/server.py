ad01920e027eb1ab514385bfade9962237ce85ef
EMMELINEMARTENS <emmemart1@student.arteveldehs.be>
2020-06-04 16:36:48 +0200

    interfaces+server

---
+++ app/flask/server.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.insert(0,'../app/print')
+from flask import Flask, render_template,request
+
+#app
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def printcommando():
+    return render_template('index.html')
+
+@app.route('/succes', methods=['GET','POST'])
+def succes():
+    return render_template('succes.html')
+
+#connection with server
+if __name__  == "__main__":
+	app.run(host='127.0.0.1',port="8000", debug=True)
