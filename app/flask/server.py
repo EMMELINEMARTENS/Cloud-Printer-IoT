e4eb36aabb552c6027330b525949575c25d2579d
EMMELINEMARTENS <emmemart1@student.arteveldehs.be>
2020-06-07 00:50:48 +0200

    cookie+fileophalen+uploadsmap

--- app/flask/server.py
+++ app/flask/server.py
@@ -1,17 +1,73 @@
 import sys
-sys.path.insert(0,'../app/js')
-from flask import Flask, render_template,request
+sys.path.insert(0,'../app/print')
+from flask import Flask, render_template, request, redirect
+import os 
+
+#security 
+from werkzeug.utils import secure_filename
+
 
 #app
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+#config path
+#/home/pi/Desktop/python/Cloud-Printer-IoT/app/flask/static/images/uploads
+app.config["IMAGE_UPLOADS"] = "/home/pi/Desktop/python/Cloud-Printer-IoT/app/flask/static/images/uploads"
+
+#extentions
+app.config["ALLOWED_EXTENSIONS"]=["PNG","JPG","JPEG","PDF","TXT"]
+
+#function to check extebtions files
+def allowed_files(filename):
+
+    if not "." in filename:
+        return False
+    else:
+        ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_EXTENSIONS"]:
+        return True
+
+    else:
+        return False
+
+@app.route('/', methods=["GET","POST"])
 def printcommando():
+
+    if request.method == "POST":
+        if request.files:
+
+            file = request.files["file"]
+    
+    #checking 
+            if file.filename == "":
+                print("File is ready for print")
+                return redirect(request.url)
+            
+            if not allowed_files(file.filename):
+                print("That file extention is not allowed")
+                return redirect(request.url)
+            
+            else:
+                filename = secure_filename(file.filename)
+        
+            
+
+
+            
+            file.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+
+            print("Image is saved")
+
+            return redirect(request.url)
+
     return render_template('index.html')
 
 @app.route('/succes', methods=['GET','POST'])
 def succes():
     return render_template('succes.html')
+
+
 
 #connection with server
 
