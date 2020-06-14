417fc98410dce2c32e8d6b08eb1018e07b5a20f6
EMMELINEMARTENS <emmemart1@student.arteveldehs.be>
2020-06-14 20:21:27 +0200

    herokuapp

--- app/flask/server.py
+++ app/flask/server.py
@@ -119,4 +119,7 @@ def succes():
 
 
 if __name__  == "__main__":
-	app.run(host='127.0.0.1',port="8080", debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    
+app.run(host='127.0.0.1',port=port, debug=True)
+    
